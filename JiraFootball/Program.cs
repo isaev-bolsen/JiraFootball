2fa287d04c19752ce95c7bba583270fbb384957b
isaev-bolsen <al_mamistov@mail.ru>
2019-11-30 12:53:31 +0300

    Base variant with Attlassian.SDK

--- JiraFootball/Program.cs
+++ JiraFootball/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Atlassian.Jira;
+using System;
 
 namespace JiraFootball
 {
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Jira jira = Jira.CreateRestClient("https://jira..com", "", "");
+			foreach (Issue issue in jira.Issues.GetIssuesFromJqlAsync("").Result)
+			{
+				Console.WriteLine(issue.Description);
+				if(string.IsNullOrWhiteSpace( issue.Description))
+				{
+					jira.Issues.AssignIssueAsync(issue.Key.Value, issue.Reporter);
+				}
+			}
+		}
+	}
 }
