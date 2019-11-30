e2ff1de30034d04d1aa259cd6f8a3576735e2617
drbolsen <al_mamistov@mail.ru>
2019-11-30 11:02:27 +0300

    initial

---
+++ JiraFootball/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JiraFootball
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
