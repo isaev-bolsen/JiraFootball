c095bb2d327cb250ba92c10e4713e7cd45ab0d91
drbolsen <al_mamistov@mail.ru>
2019-11-30 17:22:02 +0300

    condition and logging

--- JiraFootball/Program.cs
+++ JiraFootball/Program.cs
@@ -1,21 +1,26 @@
-﻿using Atlassian.Jira;
-using System;
+﻿using System;
+using Atlassian.Jira;
 
 namespace JiraFootball
 {
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-			Jira jira = Jira.CreateRestClient("https://jira..com", "", "");
-			foreach (Issue issue in jira.Issues.GetIssuesFromJqlAsync("assignee = currentUser() AND resolution = Unresolved order by updated DESC").Result)
-			{
-				Console.WriteLine(issue.Description);
-				if(string.IsNullOrWhiteSpace( issue.Description))
-				{
-					jira.Issues.AssignIssueAsync(issue.Key.Value, issue.Reporter);
-				}
-			}
-		}
-	}
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Jira jira = Jira.CreateRestClient("https://jira..com", "", "");
+            foreach (Issue issue in jira.Issues.GetIssuesFromJqlAsync("assignee = currentUser() AND resolution = Unresolved order by updated DESC").Result)
+            {
+                if (DescriptionIsWrong(issue))
+                {
+                    Console.WriteLine($"Changing Assignment for {issue.Key} due to wrong description");
+                    jira.Issues.AssignIssueAsync(issue.Key.Value, issue.Reporter);
+                }
+            }
+        }
+
+        private static bool DescriptionIsWrong(Issue issue)
+        {
+            return issue.Description.Trim().Length < 5; 
+        }
+    }
 }
